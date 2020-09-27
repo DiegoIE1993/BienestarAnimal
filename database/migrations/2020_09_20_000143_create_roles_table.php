591d7b1a84150d13dd8338f4840f7777d44572f2
Jorge Arango Cardona <jorgearango.ja@gmail.com>
2020-09-26 22:54:52 -0500

    cambio menuAdminLte

--- database/migrations/2020_09_20_000143_create_roles_table.php
+++ database/migrations/2020_09_20_000143_create_roles_table.php
@@ -14,9 +14,23 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            
+            $table->increments('id');
             $table->string('nombre');
             $table->timestamps();
+        });
+
+        Schema::create( 'role_user', function (Blueprint $table) {
+            
+            $table->increments('id');
+            $table->string('nombre');
+            $table->timestamps();
+
+            $table->unsignedInteger('user_id'); 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedInteger('role_id'); 
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
