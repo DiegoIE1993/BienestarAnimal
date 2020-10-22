74fed4a661d54072a8eda51f984eb6cdc7b378d7
Jorge Arango Cardona <jorgearango.ja@gmail.com>
2020-10-21 21:28:26 -0500

    contactanos 2

--- resources/views/educacion/show.blade.php
+++ resources/views/educacion/show.blade.php
@@ -32,10 +32,10 @@
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
                     <div class="navbar-nav ml-auto text-center">
                         <a class="nav-link active" href="/">Inicio <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="{{url('#')}}">Adopción</a>
                         <a class="nav-link" href="{{url('educacion/show')}}">Educacion</a>
                         <a class="nav-link" href="{{url('nosotros/show')}}">Nosotros</a>
                         <a class="nav-link" href="{{url('eventos/show')}}">Eventos</a>
-                        <a class="nav-link" href="{{url('/')}}"></a>
                     </div>
                 </div>
             </nav>
