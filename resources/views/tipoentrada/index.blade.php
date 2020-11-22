389909b313fe2fa48fa5056f74c655319dd911f2
DiegoOspina1993 <diegoospina1993@gmail.com>
2020-11-22 15:40:36 -0500

    datatable

--- resources/views/tipoentrada/index.blade.php
+++ resources/views/tipoentrada/index.blade.php
@@ -2,8 +2,6 @@
 @section('plugins.Datatables',true)
     
 
-<<<<<<< HEAD
-
 @section('content')
     <div class="card">
         <div class="card-body">
@@ -28,41 +26,6 @@
                     @endforeach
                 </tbody>
             </table>
-=======
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Lista Tipos de Entrada</div>
-                        <div class="card-body">
-                            <div class="row justify-content-end pb-2">
-                            <a href="{{url('/tipoentrada/create')}}" class="btn btn-success">Nuevo Registro</a>
-                            </div>
-                            <table class="table">
-                                <thead>
-                                    <th >Nombre Tipo Entrada</th>
-                                    <th >Acciones</th>
-                                </thead>
-
-                                <tbody>
-                                    @foreach ($entradas as $entrada)
-                                            <tr>
-                                                <td> {{$entrada->nombre}}</td>         
-                                                <td>
-                                                    <a href="{{ url('/tipoentrada/'.$entrada->id.'/edit')}}" class="btn btn-primary">Editar</a>
-                                                    @include('tipoentrada.delete', ['entrada' => $entrada])  
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                </tbody>
-                            </table>
-                            {{$entradas->links()}} 
-                        </div>
-                    </div>
-                </div>
-            </div> 
->>>>>>> 308dd86b05353a2e66ceb3d0e144378be56fbf6e
         </div>
     </div>
    
