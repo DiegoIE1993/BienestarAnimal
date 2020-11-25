e014161911816acf8ad4153c788e0ff878e74824
DiegoOspina1993 <diegoospina1993@gmail.com>
2020-11-24 20:46:29 -0500

    Reportes Registrar Mascota

--- resources/views/detallesadopciones/index.blade.php
+++ resources/views/detallesadopciones/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">Animales en Adopcion</div>
 
                     <div class="card-body">
-                            <table class="table">
+                            <table class="table table-striped" id="DetalleAdopcion">
                                 <thead>
                                     <th>Codigo Animal</th>
                                     <th>Foto</th>
@@ -48,5 +48,39 @@
                 
             </div>
         </div>
+        <script>
+            $(document).ready(function() {
+                $('#DetalleAdopcion').DataTable({
+                    responsive: true,
+                    autoWidth:false,
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'excel', 
+                        'pdf', 
+                        'print'
+                    ],
+                    "language": {
+                            "lengthMenu": "Mostrar " + 
+                                      `<select class="custom-select custom-select-sm form-control form-control-sm">
+                                        <option value = '10'>10</option>
+                                        <option value = '25'>25</option>
+                                        <option value = '50'>50</option>
+                                        <option value = '100'>100</option>
+                                        <option value = '-1'>All</option>
+                                       </select>` +
+                                      " registros por pagina",           
+                        "zeroRecords": "Nada encontrado - lo siento",
+                        "info": "Página _PAGE_ de _PAGES_",
+                        "infoEmpty": "No records available",
+                        "infoFiltered": "(filtrado de _MAX_ registros totales)",
+                        "search": "Buscar:",
+                        "paginate": {
+                            "next": "Siguiente",
+                            "previous": "Anterior",
+                        }
+                    }
+                });
+            } );
+        </script>
         
     @endsection 
