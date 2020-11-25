fd27356887a8b34a7ddb05e8af47b157e1df6857
DiegoOspina1993 <diegoospina1993@gmail.com>
2020-11-24 21:06:22 -0500

    Reporte Prueba

--- resources/views/detallesadopciones/index.blade.php
+++ resources/views/detallesadopciones/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Animales en Adopcion</div>
+                    <div class="card-header">Animales Adopcion</div>
 
                     <div class="card-body">
                             <table class="table table-striped" id="DetalleAdopcion">
